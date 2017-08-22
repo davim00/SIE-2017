f113d5295737a0b6351f0177906c3eb98fef4b0e
Matthew Davis <matt.davis0207@gmail.com>
2017-08-22 17:30:09 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,213 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // Concatenation
+    concat: {
+      dist: {
+        src: [
+          'js/bootstrap.js',
+          'js/theme.js'
+        ],
+        dest: 'js/scripts.js'
+      }
+    },
+    // Uglify
+    uglify: {
+      build: {
+        src: 'js/scripts.js',
+        dest: 'js/scripts.min.js'
+      }
+    },
+    // Optimize images
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/build/'
+        }]
+      }
+    },
+    // SASS
+    sass: {
+      dist: {
+        options: {
+          trace: true,
+          style: 'compressed',
+          precision: 8
+        },
+        files: {
+          'skin.css': 'sass/skin.scss'
+        }
+      },
+      build: {
+        options: {
+          trace: true,
+          style: 'compressed',
+          precision: 8
+        },
+        files: {
+          'build/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    // Autoprefixer
+    autoprefixer: {
+      dist: {
+        files: {
+          'skin.css': 'skin.css'
+        }
+      },
+      build: {
+        expand: true,
+        cwd: 'build',
+        src: [ '**/*.css' ],
+        dest: 'build'
+      }
+    },
+    // Copy
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            src: [
+              '**',
+              '!**/Containers/**',
+              '!**/node_modules/**',
+              '!**/sass/**',
+              '!**/HTML/**',
+              '!**/*.scss',
+              '!.gitignore',
+              '!.jshintignore',
+              '!.jscsrc',
+              '!_jshintignore',
+              '!_jscsrc',
+              '!.gitignore',
+              '!Gruntfile.js',
+              '!LICENSE',
+              '!package.json',
+              '!README.md',
+              '!*.dnn',
+              '!License.txt',
+              '!ReleastNotes.txt',
+              '!LICENCE',
+              '!README.md',
+              '!.sass-cache/',
+              '!*.map'
+            ],
+            dest: 'build/Skin/'
+          },
+          {
+            expand: true,
+            src: [
+              '**/Containers/**',
+              '*.dnn',
+              'License.txt',
+              'ReleastNotes.txt',
+            ],
+            dest: 'build/'
+          },
+        ],
+      },
+    },
+    // Zip
+    zip: {
+      build: {
+        files: [
+          {
+            src: ['build/Containers/*'],
+            dest: 'build/Containers.zip'
+          },
+          {
+            src: ['build/Skin/*'],
+            dest: 'build/Skin.zip'
+          },
+        ],
+      },
+    },
+    // Clean
+    clean: {
+      build: {
+        src: [ 'build' ]
+      },
+      stylesheets: {
+        src: [ 'build/**/*.css', '!build/**/style.css' ]
+      },
+      scripts: {
+        src: [ 'build/**/*.js', '!build/**/scripts.js' ]
+      },
+      templates: {
+        src: [ 'build/**/Containers/**', 'build/**/Skin/**' ]
+      },
+    },
+    // Watch
+    watch: {
+      options: {
+        livereload: true
+      },
+      ascx: {
+        files: ['*.ascx', '**/*.ascx'],
+        options: {
+          spawn: false
+        }
+      },
+      html: {
+        files: ['*.html', '**/*.html'],
+        options: {
+          spawn: false
+        }
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['concat'],
+        options: {
+          spawn: false
+        }
+      },
+      sass: {
+        files: ['sass/*.scss', 'sass/theme/*.scss'],
+        tasks: ['sass:dist'],
+        options: {
+          spawn: false
+        }
+      },
+      css: {
+        files: ['style.css'],
+        tasks: ['autoprefixer:dist']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-zip');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Register tasks
+  grunt.registerTask('default', 'watch');
+  grunt.registerTask(
+    'stylesheets',
+    'Compiles the stylesheets',
+    [ 'sass:build', 'autoprefixer:build', 'clean:stylesheets' ]
+  );
+  grunt.registerTask(
+    'templates',
+    'Compiles the resource files',
+    [ 'clean:templates' ]
+  );
+  grunt.registerTask(
+    'scripts',
+    'Compiles the JavaScript files',
+    [ 'clean:scripts' ]
+  );
+  grunt.registerTask(
+    'build',
+    'Compiles all of the assets and copies the files to the build directory',
+    [ 'clean:build', 'copy', 'stylesheets', 'scripts', 'templates' ]);
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+MDD DNN Starter
+===
+
+This is a minimal DNN starter theme for Matt Davis Design that takes care of mundane tasks when starting theme building. It includes untouched Bootstrap 3 CSS and a few more template files to help in fleshing out themes.
+
+* Bootstrap SCSS included, along with all the components.
+* Bootstrap navigation templates for the DDR Menu.
+* Grunt file and node_modules to make automation easier.
+* Licensed under GPLv2 or later. :) Use it to make something cool.
+
+Getting Started
+---------------
+
+You can start coding your files as regular HTML, however you would like. There is an "HTML" folder available to hold these files. Once completed, copy the relevant code to the ASCX templates provided, using necessary skin objects and content panes.
+
+Once you are satisfied, edit the *.dnn file with your information, as well as the ReleaseNotes.txt and License.txt. Next, run <code>grunt build</code> to create the theme installation file. This will create a "build" directory whose contents you can zip up into the installation file.
+
+Installation
+------------
+
+1. Install DNN either locally or on a testing server.
+2. Install the [40Fingers Stylehelper](https://www.40fingers.net/Products/DNN-Stylehelper) skin object.
+3. Install your new theme and apply to your site's pages to test.
+
+Now you're ready to go! The next step is easy to say, but harder to do: make an awesome DNN theme. :)
+
+Good luck!
